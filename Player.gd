cbd2aab7073b4a445a3b05ef43f282b4b1812f9c
Your Name <you@example.com>
2022-10-06 19:31:43 +0300

    liikkuminen

---
+++ Player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody
+
+var run_speed : float = 8.0
+var sidestep_speed : float = 5.0
+var velocity := Vector3()
+
+var time : float = 0.0
+var step_freq : float = 2.0 #kuinka usein
+var step_height : float = 0.2 #kuinka korkealle
+var step_tilt : float = 8.0 #degress
+
+onready var body_hinge = $BodyHinge
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	body_hinge.translation.y = step_height * (1 + sin(2.0 * PI * step_freq * time))
+	body_hinge.rotation_degrees.z = step_tilt * sin(PI * step_freq * time)
+	time += delta
+	
+	var sideways : float = 0.0
+	
+	if Input. is_action_pressed("move_right"):
+		sideways += 1.0
+	
+	if Input. is_action_pressed("move_left"):
+		sideways -= 1.0
+	
+	velocity.x = sideways *sidestep_speed
+	velocity.z = -run_speed
+	
+	move_and_slide(velocity)
