5432af220f0840410eb294faeb03f2d267047eb5
Your Name <you@example.com>
2022-12-08 19:33:40 +0200

    ui started

--- Main.gd
+++ Main.gd
@@ -4,15 +4,14 @@ var run_speed : float = 8.0
 var jump_length : float = 5.5
 var jump_height : float = 2.0
 
-var initial_road_count : int = 5
+var initial_road_count :int = 5
 var road_scenes = [
 	load("res://Road1.tscn"),
 	load("res://Road2.tscn"),
 	load("res://Road3.tscn"),
-	load("res://Road4.tscn"),
 ]
 onready var player = $Player
-onready var camera_pivot = $camera_pivot
+onready var camera_pivot = $Camera_Pivot
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,7 +22,7 @@ func _ready():
 		road.translation.z = -(i+1) * RoadBase.LENGTH
 		add_child(road)
 
-# warning-ignore:unused_argument
+
 func _physics_process(delta):
 	if player.translation.z < -RoadBase.LENGTH:
 		player.translation.z += RoadBase.LENGTH
@@ -33,12 +32,14 @@ func _physics_process(delta):
 			if road:
 				road.translation.z += RoadBase.LENGTH
 				if road.translation.z > RoadBase.LENGTH:
-					road.queue_free()
-		
+					road.queue_free() 
+		var new_road := make_random_road()
+		new_road.translation.z = initial_road_count * -RoadBase.LENGTH
+		add_child(new_road)
 	camera_pivot.translation = player.translation
 	camera_pivot.translation.y = 0
 
-func make_random_road():
+func make_random_road() -> RoadBase:
 	var road_scene = road_scenes[randi() % road_scenes.size()]
 	var road = road_scene.instance()
 	return road
